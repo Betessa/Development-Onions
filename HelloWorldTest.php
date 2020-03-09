851ab2f5a0d89453f27e50116d9461b297317162
jwubites23 <44243777+jwubites23@users.noreply.github.com>
2020-03-09 14:26:51 +0200

    Update HelloWorldTest.php

--- HelloWorldTest.php
+++ HelloWorldTest.php
@@ -1,7 +1,11 @@
 <?php
 
 class HelloWorldTest extends \PHPUnit_Framework_TestCase {
-	
+	public function testGreeting(){
+	$greeting = "Hello World";
+	$requiredGreeting = "Hello World";
+	$this->assertEquals($greeting, $requiredGreeting);	
+	}
 }
 
 ?>
