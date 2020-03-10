0f9a909545dccdee3bc062b8edb8929162f67b69
micanipho <nqmasilela777@gmail.com>
2020-03-10 14:14:33 +0200

    testing

--- HelloWorldTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class HelloWorldTest extends \PHPUnit_Framework_TestCase {
-	$greeting = "Hello World";
-	$requiredGreeting = "Hello World";
-	$this->assertEquals($greeting, $requiredGreeting);
-}
-
-?>
